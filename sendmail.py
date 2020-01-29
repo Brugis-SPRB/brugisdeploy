00843341ed7bbbc53663f8bd1a74edf243a58dee
root <root@SVDCL-BRUG01.sta.srv.cirb.lan>
2020-01-29 13:26:20 +0100

    sendmail will send file content of passed file argument

--- sendmail.py
+++ sendmail.py
@@ -1,8 +1,12 @@
 
 import smtplib
 import sys
+import os
+import datetime
 import urllib2
 import time
+
+import MyBruGISDeployConf as MDC
 
 def send_mail(subject,body):
     sender = 'brugis@urban.brussels'
@@ -23,10 +27,15 @@ def send_mail(subject,body):
         print "Error : Unable to send email", sys.exc_info[0]
 
 if __name__ == "__main__":
-    for cpt in range(60):
-        body = urllib2.urlopen("http://www.iheartquotes.com/api/v1/random").read()
-        send_mail("Your random Quote by a (not so?) funny? consultant",body)
-        time.sleep(1)
+    args = sys.argv
+    if len(args) > 1:
+	with open(os.path.join(os.path.dirname(__file__), args[1]), 'r') as body:
+            send_mail('%s - %s - mail content of %s - %s' % (MDC.localServerName, os.path.basename(__file__), args[1], str(datetime.datetime.today())), body.read())
+    else:
+        for cpt in range(60):
+            body = urllib2.urlopen("http://www.iheartquotes.com/api/v1/random").read()
+            send_mail("Your random Quote by a (not so?) funny? consultant",body)
+            time.sleep(1)
 
     
         
